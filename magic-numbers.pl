f28c658f96c4eb1615c773789d0d426cf614f32b
Daniel Marjamäki <danielm77@spray.se>
2012-01-03 22:53:50 +0100

    magic numbers: simple handling of block comments

--- magic-numbers.pl
+++ magic-numbers.pl
@@ -14,9 +14,24 @@ sub checkfile
 
     # check comments..
     my $linenr = 0;
+    my $comment = 0;
     foreach $line (@lines)
     {
         $linenr = $linenr + 1;
+
+        # remove block comments..
+        if ($line =~ /^\s*\/\*+\s*$/)
+        {
+            $comment = 1;
+        }
+        if ($line =~ /\*\//)
+        {
+            $comment = 0;
+        }
+        if ($comment == 1)
+        {
+            $line = "";
+        }
 
         # is there a magic number?
         if (($line =~ /[^a-zA-Z0-9_][0-9]{3,}/) && 
